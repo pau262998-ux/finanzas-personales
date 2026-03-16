cb0981001aa767c36b993dc24b9e1b28a4087ea6
pau262998-ux <pau262998@gmail.com>
2026-03-15 22:12:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,7 @@ if archivo is not None:
  if archivo.name.endswith(".csv"):
   try: df=pd.read_csv(archivo,encoding="latin1",sep=",")
   except: archivo.seek(0)
- df=pd.read_csv(archivo,encoding="latin1",sep=";")
- else: df=pd.read_excel(archivo)
+ df=pd.read_csv(archivo,encoding="latin1",sep=";") else: df=pd.read_excel(archivo)
 
  if df.shape[1]==1:
   df=df[df.columns[0]].str.split(",",expand=True)
